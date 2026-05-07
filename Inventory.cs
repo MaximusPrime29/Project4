a5eb190df145355c64608b06292d3d45387b3860
Qnchitu <cosiyankov@gmail.com>
2026-05-07 10:57:03 +0200

    First Steps

---
+++ Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Inventory
+{
+	public int InventoryId { get; set; }
+	public Product Product { get; set; }
+	public int StoreID { get; set; }
+	public int Quantity { get; set; }
+    public int MinimumQuantity { get; set; }
+    public DateTime ExpiryDate { get; set; }
+
+    public Inventory(int inventoryId, int productId, int storeID, int quantity, int minQuantity, DateTime expiryDate)
+	{
+		InventoryId = inventoryId;
+		ProductId = productId;
+		StoreID = storeID;
+		Quantity = quantity;
+        MinimumQuantity = minQuantity;
+        ExpiryDate = expiryDate;
+    }
+    public bool IsExpiringSoon()
+    {
+        return ExpiryDate <= DateTime.Now.AddDays(3);
+    }
+
+    public bool IsLowStock()
+    {
+        return Quantity <= MinimumQuantity;
+    }
+}
---
+++ Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Store
+{
+	public int StoreId { get; set; }
+	public string StoreName { get; set; }
+	public string StoreLocation { get; set; }
+
+	public Store( int storeId, string storeName, string storeLocation)
+	{
+		StoreId = storeId;
+		StoreName = storeName;
+		StoreLocation = storeLocation;
+	}
+}
