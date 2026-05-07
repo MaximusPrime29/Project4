a2fd6e6c4b4281e20dec20f548a05d73ce213ede
Max Schulte <142296959+MaximusPrime29@users.noreply.github.com>
2026-05-07 14:05:49 +0200

    Transfer scripts and logic

---
+++ Project 4/Project 4/Models/Transfer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Transfer
+{
+    public int TransferId { get; set; }
+
+    public int FromStoreId { get; set; }
+
+    public int ToStoreId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTime TransferDate { get; set; }
+
+    public Transfer(int transferId, int fromStoreId, int toStoreId, int productId, int quantity)
+    {
+        TransferId = transferId;
+        FromStoreId = fromStoreId;
+        ToStoreId = toStoreId;
+        ProductId = productId;
+        Quantity = quantity;
+        TransferDate = DateTime.Now;
+    }
+}
---
+++ Project 4/Project 4/Models/TransferRequest.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+public class TransferRequest
+{
+    public int FromStoreId { get; set; }
+
+    public int ToStoreId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int Quantity { get; set; }
+}
